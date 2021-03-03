7cfa02d63d1a142692d9a25a8b5329f8224f6339
Nuwidra <https://github.com/Nuwidra/IC-2101-POO-2020-ii->
2021-03-02 20:10:23 -0600

    Pruebas

--- src/main/java/tec/bd/app/service/EstudianteService.java
+++ src/main/java/tec/bd/app/service/EstudianteService.java
@@ -26,18 +26,47 @@ public class EstudianteService {
 
     public void addNew(Estudiante e) {
         // Verificar si el estudiante que viene por parametro ya existe en la BD
-        this.database.getEstudianteTable().add(e);
+        boolean estado = false;
+        var tabla = this.database.getEstudianteTable();
+        for(Estudiante persona : tabla){
+            if(persona.getCarne() == e.getCarne()){
+                estado = true;
+                break;}}
+        if (!estado){
+            this.database.getEstudianteTable().add(e);
+        }else {
+            System.out.println("\n==El estudiante ya esta reguistrado==");}
     }
 
     public void updateStudent(Estudiante e) {
         // Implementar codigo de actualizacion
         // Traer el estudiante con carne X
         // modificar el estudiante con los datos del estudiante e
+        boolean estado = false;
+        var tabla = this.database.getEstudianteTable();
+        for (Estudiante persona : tabla){
+            if (persona.getCarne() == e.getCarne()){
+                persona.setNombre(e.getNombre());
+                persona.setApellido(e.getApellido())
+                ;persona.setEdad(e.getEdad());
+                estado = true;
+            }
+        }if (!estado){
+            System.out.println("\n==La persona no existe en la base==");
+        }
     }
-
     public void deleteStudent(long carne) {
         // implementar codigo de borrado
         // HashSet.remove(e);
+        boolean estado = false;
+        var tabla = this.database.getEstudianteTable();
+        for (Estudiante persona : tabla) {
+            if (persona.getCarne() == carne){
+                this.database.getEstudianteTable().remove(persona);
+                estado = true;
+            }
+        }if (!estado){
+            System.out.println("\n==La persona no existe en la base==");
+        }
     }
-
 }
